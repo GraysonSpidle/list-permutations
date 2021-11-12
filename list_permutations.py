8a1492b257fc445fa7e36e15f288f22f9d41e031
Grayson Spidle <graysonkc@gmail.com>
2021-11-12 14:25:41 -0600

    Added some functions I'm using for testing.
    These functions range from graph theory functions to a very useful function that enumerates all the end indices in thing.dat
    
    Which is great since those end indices are the ones that cause the biggest slowdowns.

--- list_permutations.py
+++ list_permutations.py
@@ -35,7 +35,7 @@ def fu(n): # This isn't ever used in this file, but I'll leave it here in case i
     return sum(path_size(n, get_unique_path_index(n, i)) for i in range(G(n))) # I don't have an equation for this one
 
 def F(n): # This isn't ever used in this file, but I'll leave it here in case it becomes useful later.
-    ''' Number of unique nodes across all paths in the raw series (Yes the raw series not the unique series). O(1). '''
+    ''' Number of unique nodes across all paths in the raw series (Yes the raw series not the unique series). O(n). '''
     return ((n - 1) * n // 2) - 1 # it's a triangle number - 1
 
 def Fu(n): # This isn't ever used in this file, but I'll leave it here in case it becomes useful later.
@@ -55,13 +55,13 @@ def G(n):
 # Raw Series Functions =========================================================
 
 def depth(n, i):
-    ''' Returns the depth of the path at index i. Do not put 0 for i, as it will always return an incorrect number. O(n^2). '''
+    ''' Returns the depth of the path at index i. Do not put 0 for i, as it will always return an incorrect number. '''
     I = (i - 1) % g(n - 1)
     if I and n > 3:
         return 1 + depth(n - 1, I)
     return 0
 
-def path_size(n, i): # O(n^2)
+def path_size(n, i):
     return n - (1 + depth(n, i)) * bool(i) # i = 0 returns n
 
 def normalize(n, i):
@@ -131,7 +131,8 @@ def _func2(i):
             return 1 << k #2**k
 
 def get_unique_path_index(n, i):
-    ''' Returns an int that is the index param for get_path() for the ith element that is unique. Does include input. '''
+    ''' Returns an int that is the index param for get_path() for the ith element that is unique. Does include input. 
+    O(n^2) '''
     if n < 5:
         if n == 4:
             return (0,1,2,3,4,6,7)[i]
@@ -141,7 +142,7 @@ def get_unique_path_index(n, i):
     # past this point we need to offset i by n.
     num = 0
     i -= n
-    while i > 0:
+    while i > 0: # optimize this!
         num += _func(i)
         i -= _func2(i)
     
@@ -150,26 +151,51 @@ def get_unique_path_index(n, i):
     elif i == 0:
         return num + n
 
+# Experimental Functions ======================================================
+
+def end_indices():
+    ''' 
+    So this function will generate all the end indices as seen in thing.dat. 
+    That is, the indices that are on the end of each row in thing.dat.
+
+    If someone were to find the inverse of this, it would save sooooo
+    much time during computations because the end indices are the slowest
+    ones to calculate which would bring the big O down to linear scaling.
+    '''
+
+    for i in count(0):
+        y = int( ((8*i + 17)**0.5 -5) // 2 + 1 )
+        yield (1 << (y + 3)) - y - 4 - (1 << ( (y*(y+5)) // 2 + 1 - i ) )
+
 # Endpoint Functions ===========================================================
 
-def paths(n):
+def paths(n, r=None):
     ''' Returns a generator that yields every single path (with lots of duplicates) for a string of size n. '''
     for i in range(g(n)):
-        yield get_path(n, i)
+        if r is None or path_size(n, i) in r:
+            yield get_path(n, i)
 
-def unique_paths(n):
+def unique_paths(n, r=None):
     ''' Returns a generator that yields every single path (with no duplicates) for a string of size n. '''
     for i in range(G(n)):
-        yield get_path(n, get_unique_path_index(n, i))
+        I = get_unique_path_index(n, i)
+        if r is None or path_size(n, I) in r:
+            yield get_path(n, I)
 
 def apply_path(sliceable, path):
     ''' Returns a generator that yields each sliced node the path creates with the sliceable object. '''
     for node in path:
         yield sliceable[slice(*node)]
 
-def iterate_paths(sliceable):
-    for upath in unique_paths(len(sliceable)):
+def iterate_paths(sliceable, r=None):
+    for upath in unique_paths(len(sliceable), r):
         yield apply_path(sliceable, upath)
+
+def generate_templates_unique(n, r=None):
+    for i in range(1, G(n)):
+        I = get_unique_path_index(n, i)
+        if r is None or path_size(n, I) in r:
+            yield tuple(generate_transform_indices(n, I))
 
 # Main =========================================================================
 
@@ -183,9 +209,19 @@ def make_list_diagram_raw(n):
 def make_list_diagram_unique(n):
     with open('%d.dat' % n, 'w') as file:
         for i in range(G(n)):
+            I = get_unique_path_index(n, i)
             if i:
-                file.write("    " * depth(n, i))
-            file.write(" ".join(str(e) for e in get_path(n, i)) + '\n')
+                file.write("    " * depth(n, I))
+            file.write(" ".join(str(e) for e in get_path(n, I)) + '\n')
+
+def make_list_diagram_transform_indices_unique(n):
+    with open('%d.dat' % n, 'w') as file:
+        for i in range(G(n)):
+            I = get_unique_path_index(n, i)
+            if not i:
+                continue
+            file.write("    " * depth(n, I))
+            file.write(" ".join(str(e) for e in generate_transform_indices(n, I)) + '\n')
 
 def indices(n, z):
     if z == n:
@@ -196,6 +232,84 @@ def indices(n, z):
         if path_size(n, I) == z:
             yield i
 
+def t(n):
+    return (n**2) // 2 - (3 * n) // 2 - 1
+
+def thing(i):
+    if i:
+        return ((1 << (i + 3)) - 1) ^ 3
+    return 1
+
+def is_based_on_zero(i) -> bool:
+    return bool((log2(i + 3) - 2) % 1)
+
+def generate_thing_dat(n):
+    yield [0]
+    i = 1
+    for p in range(1, n):
+        for P in range(p, -1, -1):
+            temp = []
+            for k in range(2**P):
+                temp.append(i)
+                i += 1
+            yield temp
+
+def write_thing_dat(n):
+    length = len(str(2**(n+1) - (3+n)))
+    with open("thing.dat", 'w') as file:
+        for line in generate_thing_dat(n):
+            formatted_line = " ".join((["{:%dd}" % (length)] * len(line)))
+            file.write(formatted_line.format(*line) + "\n")
+
+
+
+def get_children(i):
+    output = []
+    for k, line in enumerate(mat):
+        if line[i]:
+            output.append(k)
+    return output
+
+def find_parents(i):
+    parents = []
+    for k in range(i+1, len(mat)):
+        if i in get_children(k):
+            parents.append(k)
+            if len(parents) == 2:
+                return parents
+    return parents
+
+def is_it_one(i):
+    parents = find_parents(i)
+    if len(parents) == 1:
+        return False
+    thing = mat[i]
+    while any(thing):
+        if thing.count(True) > 1:
+            return False
+        i = thing.index(True)
+        thing = mat[i]
+    return True
+
+H = lambda n,c: int(log2(n + 3 + c)) - int(log2(n+2+c))
+def H2(k, n):
+    if k < 2:
+        return list(filter(lambda e: H(e, k), range(n)))[k:]
+    return list(filter(lambda e: H(e, k), range(n)))[k-1:]
+
+def is_pathological(i):
+    int(log2(i + 3))
+
 if __name__ == "__main__":
-    for e in iterate_paths("birds"):
-        print(*e)
+    write_thing_dat(9)
+    print("done")
+    n = G(15)
+
+    # col to row
+    mat = list(list(False for k in range(n)) for i in range(n))
+
+    for i in range(2, n):
+        mat[i - _func2(i)][i] = True
+    
+
+    print()
